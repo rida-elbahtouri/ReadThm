4b083402fe5d81b1c4d41a7f9facb1f3cb8f015e
rida-elbahtouri <elbahtouri0123@gmail.com>
2021-07-27 09:24:31 +0100

    add some styling to result page

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -1,23 +1,16 @@
-//import {useState,useEffect} from 'react';
-// import {searchForBlog} from '../functions/Api';
 import { connect } from 'react-redux'; 
 import ResultCard from './blogs/ResultCard';
 import '../assets/styles/home.scss'
 const ResultPage = (props) => {
-    // useEffect(() => {
-    //     searchForBlog(props.match.params.term)
-    //     .then((res) => setSearchResult(res.data))
-    //     .catch((err) => console.log(err))
-    // },[])
-
-    // const [searchResult,setSearchResult] = useState(null)
-    console.log(props)
+  
     const renderHelpeer = (blogs) => {
-        if(blogs){
+        if(blogs && blogs.length > 0){
             const res = blogs.map(blog=>{
                 return <ResultCard key={blog.id} blog = {blog} />
             })
             return res
+        }else {
+            return <h1>There are 0 Results</h1>
         }
     }
     return (
