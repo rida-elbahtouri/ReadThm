6c4c98d0c1bf272a163852b2c9ebf3f32f4c8e46
rida-elbahtouri <elbahtouri0123@gmail.com>
2021-08-06 11:06:47 +0100

    fix images don't render

--- src/functions/checkPhoto.js
+++ src/functions/checkPhoto.js
@@ -1,22 +1,24 @@
+import {baseUrl} from './Api';
+
 export const BlogimageRender=(blog)=> {
     const addDefaultSrc =(ev)=> {
         ev.target.src = 'https://via.placeholder.com/150?text=NoPicture'
     }
-    return <img src={`http://localhost:3000/blogs/${blog.id}/photo`}
+    return <img src={`${baseUrl}/blogs/${blog.id}/photo`}
     onError={addDefaultSrc} />
    }
 export const BlogReadImage=(blog)=> {
     const addDefaultSrc =(ev)=> {
         ev.target.className = 'none'
     }
-    return <img src={`http://localhost:3000/blogs/${blog.id}/photo`}
+    return <img src={`${baseUrl}/blogs/${blog.id}/photo`}
     onError={addDefaultSrc} />
 }
 export const UserAvatarRender=(user)=> {
     const addDefaultSrc =(ev)=> {
         ev.target.src = 'https://via.placeholder.com/150?text=Avatar'
     }
-    return <img src={`http://localhost:3000/users/${user.id}/avatar`}
+    return <img src={`${baseUrl}/users/${user.id}/avatar`}
     onError={addDefaultSrc} />
 }
 export const isPhotoValid = (photo) => {
